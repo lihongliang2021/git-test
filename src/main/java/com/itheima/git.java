2cc981b1fedeba1e1f1f5e75a043379b938a3d4f
lihongliang <1454712595@qq.com>
2021-07-11 21:18:41 +0800

    qqqqqqqqqq

--- src/main/java/com/itheima/git.java
+++ src/main/java/com/itheima/git.java
@@ -10,6 +10,7 @@ public class git {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("qqqqqqqqqq");
 
             
     }
